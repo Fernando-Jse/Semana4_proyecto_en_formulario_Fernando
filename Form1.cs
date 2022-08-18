d4892b988f4e94d53491735f9e5e8b0be1997f20
Tespinsk <ferloto1304@gmail.com>
2022-08-17 19:13:58 -0600

    Aqui edite el formulario para que parezca un juego de parejas con sus filas y columnas establecidas

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Semana5_proyecto_en_formulario_Fernando
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
